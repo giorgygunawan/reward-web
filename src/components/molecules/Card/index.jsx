ef96602046a992f13232e71198e8083272f5b237
giorgy gunawan <giorgy@styletheory.co>
2019-08-13 22:40:00 +0700

    make card molecule

---
+++ src/components/molecules/Card/index.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+import classnames from 'classnames'
+import Avatar from '../Avatar'
+import Label, { Level as LabelLevel, Color as LabelColor } from '../../atoms/Label'
+import CardImage from '../CardImage'
+import './style.css'
+
+
+type Props = {
+  cardImageSource: string,
+  cardImageLabelTitle: string,
+  cardImageAlt: string,
+  avatarImageSource: string,
+  avatarImageAlt: string,
+  avatarTitle: string, 
+  avatarSubtitle: string,
+  cardTitle: string,
+  children: React.Node,
+  className: string,
+}
+
+const Card = (props: Props): Component => {
+  const { cardImageSource, cardImageLabelTitle, cardImageAlt, avatarImageSource, avatarImageAlt, avatarTitle, avatarSubtitle, cardTitle, className } = props
+  const classProps = classnames(
+    "molecules-card",
+    className
+  )
+  return (
+    <div className= {classProps}>
+      <CardImage className="molecules-card-card-image" imageSource={cardImageSource} imageAlt={cardImageAlt} labelTitle={cardImageLabelTitle} />
+      <Avatar className="molecules-card-avatar" imageSource={avatarImageSource} imageAlt={avatarImageAlt} title={avatarTitle} subtitle={avatarSubtitle} />
+      <Label className="molecules-card-title" level={LabelLevel.H1} color={LabelColor.BLACK}>
+        {cardTitle}
+      </Label>
+    </div>
+  )
+}
+
+Card.defaultProps = {
+  className: ''
+}
+
+export default Card
