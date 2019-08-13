b260a2147ee3ff15a6fbe6f5fee997edd7e95881
giorgy gunawan <giorgy@styletheory.co>
2019-08-13 22:18:59 +0700

    make card image molecule

---
+++ src/components/molecules/CardImage/index.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import classnames from 'classnames'
+import Label, { Level as LabelLevel, Color as LabelColor } from '../../atoms/Label'
+
+import './style.css'
+
+type Props = {
+  imageSource: string,
+  imageAlt: string,
+  labelTitle: string,
+  className: string,
+}
+
+const CardImage = (props: Props): Component => {
+  const { imageSource, imageAlt, labelTitle, className } = props
+  const classProps = classnames(
+    "molecules-card-image-container",
+    className
+  )
+  return (
+    <div className= {classProps}>
+        <img className="molecules-card-image" src={imageSource} alt={imageAlt} />
+        <Label color={LabelColor.WHITE} level={LabelLevel.BODY_BOLD} className="molecules-card-image-label"> {labelTitle} </Label>
+    </div>
+  )
+}
+
+CardImage.defaultProps = {
+  imageAlt: 'default alt',
+  className: ''
+}
+
+export default CardImage
