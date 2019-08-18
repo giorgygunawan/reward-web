2676acab5eb6ffbb7a07d5120216737e7593028c
giorgy gunawan <giorgy@styletheory.co>
2019-08-18 16:05:34 +0700

    make labeled checkbox molecule

---
+++ src/components/molecules/LabeledCheckBox/index.jsx
@@ -0,0 +1,50 @@
+
+import React, { Component } from 'react'
+import classnames from 'classnames'
+import PropTypes from 'prop-types'
+import Label, { Level as LabelLevel, Color} from '../../atoms/Label'
+import './style.css'
+
+
+class LabeledCheckBox extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isChecked: false,
+    };
+  }
+
+  onChangeHandler(event) {
+    console.log(event.target.value)
+  }
+
+  changeState() {
+    this.setState((prevState, props) => ({
+      isChecked: !prevState.isChecked
+    }));
+  }
+
+  render() {
+    const { title, className, onChangeHandler } = this.props
+    const classProps = classnames(
+      "molecules-labeled-check-box-container",
+      className
+    )
+    return <div className={classProps} onClick={() => {this.changeState()}}>
+        <input className="molecules-labeled-check-box" type="checkbox" checked={this.state.isChecked} onChange={(event) => this.onChangeHandler(event)}/>
+        <Label className="molecules-labeled-check-box-label" level={LabelLevel.BODY} color={Color.BLACK}>{title}</Label>
+      </div>
+  }
+}
+
+LabeledCheckBox.propTypes = {
+  title: PropTypes.string,
+  className: PropTypes.string,
+  onChangeHandler: Function
+}
+
+LabeledCheckBox.defaultProps = {
+  className: ''
+}
+
+export default LabeledCheckBox
