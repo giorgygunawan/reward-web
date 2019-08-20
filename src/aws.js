90e96c88773039943cbd37d1fd4bf51f73565486
giorgy gunawan <giorgy@styletheory.co>
2019-08-20 13:51:34 +0700

    add aws configuration

---
+++ src/aws.js
@@ -0,0 +1,12 @@
+export default {
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://teynwujdld.execute-api.us-east-1.amazonaws.com/dev/"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_0tAaMKgdK",
+    APP_CLIENT_ID: "4urf82ga4vuf8msbabafsul46c",
+    IDENTITY_POOL_ID: "us-east-1:154e5ada-1857-42ac-80a2-e7aaf2ddc114"
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Amplify from "aws-amplify";
+import AwsConfig from './aws';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import './functional.css';
 import 'font-awesome/css/font-awesome.css';
+
+Amplify.configure({
+  Auth: {
+    mandatorySignIn: true,
+    region: AwsConfig.cognito.REGION,
+    userPoolId: AwsConfig.cognito.USER_POOL_ID,
+    identityPoolId: AwsConfig.cognito.IDENTITY_POOL_ID,
+    userPoolWebClientId: AwsConfig.cognito.APP_CLIENT_ID
+  },
+  API: {
+    endpoints: [
+      {
+        name: "rewards",
+        endpoint: AwsConfig.apiGateway.URL,
+        region: AwsConfig.apiGateway.REGION
+      },
+    ]
+  }
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.register();
