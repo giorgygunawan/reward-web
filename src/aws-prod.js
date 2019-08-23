2d677b50982ba6913b0bbd660e63ba29662fc514
giorgy gunawan <giorgy@styletheory.co>
2019-08-23 10:24:11 +0700

    make prod and dev environment

---
+++ src/aws-prod.js
@@ -0,0 +1,12 @@
+export default {
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://um6qmlgp5m.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_6YixhcxYi",
+    APP_CLIENT_ID: "1mtptn9u3osrfiv67lohk929t4",
+    IDENTITY_POOL_ID: "us-east-1:937b9f35-49d3-4d96-95e7-3ad6a35c5a1f"
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,31 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Amplify from "aws-amplify";
-import AwsConfig from './aws';
+import AwsConfigDev from './aws-dev';
+import AwsConfigProd from './aws-prod';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import './functional.css';
 import 'font-awesome/css/font-awesome.css';
 
-Amplify.configure({
-  Auth: {
-    mandatorySignIn: false,
-    region: AwsConfig.cognito.REGION,
-    userPoolId: AwsConfig.cognito.USER_POOL_ID,
-    identityPoolId: AwsConfig.cognito.IDENTITY_POOL_ID,
-    userPoolWebClientId: AwsConfig.cognito.APP_CLIENT_ID
-  },
-  API: {
-    endpoints: [
-      {
-        name: "rewards",
-        endpoint: AwsConfig.apiGateway.URL,
-        region: AwsConfig.apiGateway.REGION
-      },
-    ]
+Amplify.configure(getAmplifyConfiguration());
+
+function getAmplifyConfiguration() {
+  let AwsConfig = AwsConfigDev
+  if (process.env.NODE_ENV === 'development') {
+    AwsConfig = AwsConfigDev
+  } else {
+    AwsConfig = AwsConfigProd
   }
-});
+  return {
+    Auth: {
+      mandatorySignIn: false,
+      region: AwsConfig.cognito.REGION,
+      userPoolId: AwsConfig.cognito.USER_POOL_ID,
+      identityPoolId: AwsConfig.cognito.IDENTITY_POOL_ID,
+      userPoolWebClientId: AwsConfig.cognito.APP_CLIENT_ID
+    },
+    API: {
+      endpoints: [
+        {
+          name: "rewards",
+          endpoint: AwsConfig.apiGateway.URL,
+          region: AwsConfig.apiGateway.REGION
+        },
+      ]
+    }
+  }
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.register();
